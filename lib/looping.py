89b269e2ee3299a0bbb04d25171a3d48e7cdebea
Dave J <29786887+daveswork@users.noreply.github.com>
2024-08-25 11:58:42 -0400

    All local tests pass.

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,28 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 10
+    while i >= 0:
+        if i == 0:
+            print("Happy New Year!")
+        else:
+            print(i)
+        i -= 1
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    squared_value = []
+    for i in range(len(int_list)):
+        squared_value.append(int_list[i]**2)
+    return squared_value
+    
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
